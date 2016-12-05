d83d227b3d7b5d6a89384e653a5203a90f8d2d93
Peter Anderson <diniremix@gmail.com>
2016-12-05 00:19:39 -0500

    added events for actions

--- qpassx.cpp
+++ qpassx.cpp
@@ -12,3 +12,93 @@ QpassX::~QpassX()
 {
   delete ui;
 }
+
+void QpassX::on_actionQuit_triggered()
+{
+    this->close();
+}
+
+void QpassX::on_actionNew_triggered()
+{
+
+}
+
+void QpassX::on_actionOpen_triggered()
+{
+
+}
+
+void QpassX::on_actionClear_history_triggered()
+{
+
+}
+
+void QpassX::on_actionChange_password_triggered()
+{
+
+}
+
+void QpassX::on_actionDatabase_settings_triggered()
+{
+
+}
+
+void QpassX::on_actionImport_from_triggered()
+{
+
+}
+
+void QpassX::on_actionExport_database_triggered()
+{
+
+}
+
+void QpassX::on_actionSettings_triggered()
+{
+
+}
+
+void QpassX::on_actionAbout_triggered()
+{
+
+}
+
+void QpassX::on_actionCheck_for_updates_triggered()
+{
+
+}
+
+void QpassX::on_actionFind_triggered()
+{
+
+}
+
+void QpassX::on_actionEdit_entry_triggered()
+{
+
+}
+
+void QpassX::on_actionDelete_entry_triggered()
+{
+
+}
+
+void QpassX::on_actionNew_entry_triggered()
+{
+
+}
+
+void QpassX::on_actionCopy_username_triggered()
+{
+
+}
+
+void QpassX::on_actionCopy_password_triggered()
+{
+
+}
+
+void QpassX::on_actionOpen_url_triggered()
+{
+
+}
--- qpassx.h
+++ qpassx.h
@@ -15,6 +15,43 @@ public:
   explicit QpassX(QWidget *parent = 0);
   ~QpassX();
 
+private slots:
+  void on_actionQuit_triggered();
+
+  void on_actionNew_triggered();
+
+  void on_actionOpen_triggered();
+
+  void on_actionClear_history_triggered();
+
+  void on_actionChange_password_triggered();
+
+  void on_actionDatabase_settings_triggered();
+
+  void on_actionImport_from_triggered();
+
+  void on_actionExport_database_triggered();
+
+  void on_actionSettings_triggered();
+
+  void on_actionAbout_triggered();
+
+  void on_actionCheck_for_updates_triggered();
+
+  void on_actionFind_triggered();
+
+  void on_actionEdit_entry_triggered();
+
+  void on_actionDelete_entry_triggered();
+
+  void on_actionNew_entry_triggered();
+
+  void on_actionCopy_username_triggered();
+
+  void on_actionCopy_password_triggered();
+
+  void on_actionOpen_url_triggered();
+
 private:
   Ui::QpassX *ui;
 };
