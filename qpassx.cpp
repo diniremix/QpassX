368afde2671d054c74f73a541db8b13001b260c7
Peter Anderson <diniremix@gmail.com>
2016-12-10 18:21:12 -0500

    test connection, create, and open db

--- qpassx.cpp
+++ qpassx.cpp
@@ -6,6 +6,8 @@ QpassX::QpassX(QWidget *parent) :
   ui(new Ui::QpassX)
 {
   ui->setupUi(this);
+  db = new QDatabase();
+
 }
 
 QpassX::~QpassX()
@@ -15,16 +17,59 @@ QpassX::~QpassX()
 
 void QpassX::on_actionQuit_triggered()
 {
-    this->close();
+  this->close();
 }
 
 void QpassX::on_actionNew_triggered()
 {
+  qDebug() << "on_actionNew_triggered";
+  QString fileName = QFileDialog::getSaveFileName(this,
+                                                  tr("Create new Database"), "",
+                                                  tr("QpassX Databse (*.qdbx);;All Files (*)"));
 
+
+  if (fileName.isEmpty()){
+      return;
+    }
+  else {
+      //create db
+      if(db->createDB(fileName)){
+          qDebug() << "create db";
+      }
+    }
 }
 
 void QpassX::on_actionOpen_triggered()
 {
+  qDebug() << "on_actionOpen_triggered";
+  QString fileName = QFileDialog::getOpenFileName(this,
+                                                  tr("Open QpassX QDatabase"), "",
+                                                  tr("QpassX Databse (*.qdbx);;All Files (*)"));
+  if (fileName.isEmpty()){
+      return;
+    }else {
+      // open db
+      if(db->openDB(fileName)){
+        //loadScheme();
+         QList<QString> tables, fields;
+         tables << "qpassxscheme";
+         fields << "usrnm" << "psswd";
+         //mylist.append("jorge");
+         QString qpass= "jorge1";
+         //db->select(tables, fields,"psswd="+qpass);
+         QSqlQuery result;
+
+         result= db->execQuery(tr("select usrnm, psswd from qpassxscheme where psswd='%1'").arg(qpass),"");
+         if(result.isValid()){
+             qDebug() << "result:"<<result.value(0).toString();
+         }else{
+             qDebug() << "result: fail no es valid";
+           }
+
+      }else{
+
+      };
+    }
 
 }
 
@@ -50,7 +95,7 @@ void QpassX::on_actionImport_from_triggered()
 
 void QpassX::on_actionExport_database_triggered()
 {
-
+  qDebug() << "on_actionExport_database_triggered";
 }
 
 void QpassX::on_actionSettings_triggered()
@@ -70,6 +115,14 @@ void QpassX::on_actionCheck_for_updates_triggered()
 
 void QpassX::on_actionFind_triggered()
 {
+  qDebug() << "on_actionFind_triggered";
+  //db->search("users", 1);
+  QList<QString> tables, fields;
+  //tables << "users" << "information";
+  fields << "one" << "two" << "three";
+  //mylist.append("jorge");
+
+  db->select(tables, fields,"id=1");
 
 }
 
